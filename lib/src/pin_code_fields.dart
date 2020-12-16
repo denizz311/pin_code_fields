0f8da1eb2b5774b8f0057f3ced62da0680985d67
dev <denys.loziuk@uapay.ua>
2020-12-16 11:22:07 +0200

    pin_code_fields.dart add selected border width

--- lib/src/pin_code_fields.dart
+++ lib/src/pin_code_fields.dart
@@ -245,7 +245,7 @@ class _PinCodeTextFieldState extends State<PinCodeTextField>
     _initializeValues();
 
     _cursorController = AnimationController(
-        duration: Duration(milliseconds: 1000), value: this);
+        duration: Duration(milliseconds: 1000), vsync: this);
     _cursorAnimation = Tween<double>(
       begin: 1,
       end: 0,
@@ -255,7 +255,7 @@ class _PinCodeTextFieldState extends State<PinCodeTextField>
     ));
     _controller = AnimationController(
       duration: Duration(milliseconds: widget.errorAnimationDuration),
-      value: this,
+      vsync: this,
     );
     _offsetAnimation = Tween<Offset>(
       begin: Offset.zero,
